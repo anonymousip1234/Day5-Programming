243d40f428f0e4015f972cde89ef39fec78936fb
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:36:55 +0530

    Add files via upload

---
+++ Largest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+
+    internal class Largest
+    {
+
+        public int a;
+        public int b;
+        public int c;
+        public void ThreeNumber()
+        {
+            Console.WriteLine("Enter the Three numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("The largest number is" + a);
+
+
+                }
+                else
+                {
+                    Console.WriteLine("The largest number is " + c);
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine("The largest number is" + b);
+
+                }
+                else
+                {
+                    Console.WriteLine("The largest number is" + c);
+                }
+            }
+
+        }
+    }
+
+}
+
+
+    
