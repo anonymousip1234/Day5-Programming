c4df9ba461e73e4d9986b0607a075e9988c17443
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:34:44 +0530

    Add files via upload

---
+++ Harmonic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Harmonic
+
+
+    {
+
+        public float number = 0;
+        public int i = 1;
+        public void Harmonicnum()
+        {
+            int x;
+            
+            Console.WriteLine("Enter The number");
+            x = Convert.ToInt32(Console.ReadLine());
+            
+            while(i<=x)
+            {
+                float number =  + (1 / i);
+                
+                i++;
+            }
+
+
+            Console.WriteLine("The summation is " + number);
+        }
+         
+    }
+}
