436d8e1b6c9c005994a7b1cf3edf1445a03515ad
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:38:09 +0530

    Add files via upload

---
+++ Power of 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Power_of_2
+
+    {
+
+        public int i = 1;
+        public int mainNum = 1;
+        public void Power()
+        {
+            
+              int x;
+             
+        
+            Console.WriteLine("Enter The number");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= x)
+            {
+                mainNum = mainNum * 2;
+                Console.WriteLine(mainNum);
+                i++;
+            }
+
+          
+           
+           
+        }
+        
+        
+    }
+
+}
