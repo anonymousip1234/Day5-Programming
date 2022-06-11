8089f7efda88e1854b6b1368274489edd371824b
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:37:33 +0530

    Add files via upload

---
+++ Leap Year.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Leap_Year
+    {
+        public void Leap()
+        {
+
+            int year;
+            Console.WriteLine("Enter The year");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 100 == 0)
+            {
+                Console.WriteLine(year + "Is not a Leap Year");
+
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine(year + "Is a Leap Year");
+
+                }
+                else
+                {
+                    Console.WriteLine(year + "Is not a Leap Year");
+                }
+            
+            
+            }   
+
+        }
+        
+
+    }
+}
