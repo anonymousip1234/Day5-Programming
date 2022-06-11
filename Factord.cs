22ba59bd83a2394a23d1fb995a502adf91f46897
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:33:12 +0530

    Add files via upload

---
+++ Factord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Factor
+    {
+        public int N;
+        public int i = 1;
+        public void Factors()
+        {
+            Console.WriteLine("Enter the number");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= N / 2)
+            {
+                if(N%i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+        }
+
+
+
+    }
+}
